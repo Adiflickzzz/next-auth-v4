9d3c11886a3e20fdd5aa2aab2515c08fd8113c4d
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-07-29 23:22:52 +0530

    router and login button functionality set-up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button>button</Button>;
+  return (
+    <main className="flex flex-col h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-teal-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg" className="font-bold text-xl">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
