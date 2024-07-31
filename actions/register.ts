4e62fd5c34ce22136a80672037ae93b1c34d97be
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-07-31 14:58:24 +0530

    added prisma and database , it is completely working

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
 import * as z from "zod";
-
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validationFields = RegisterSchema.safeParse(values);
@@ -10,5 +12,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields !" };
   }
 
-  return { success: "Email sent !" };
+  const { email, name, password } = validationFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use !" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send varification token 
+
+  return { success: "User created successfully !" };
 };
